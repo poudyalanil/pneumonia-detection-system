3a37db3082cdbd06effaa780be93ac72f4ce5a63
poudyalanil <anilrunjakote@gmail.com>
2020-09-23 18:04:01 +0545

    User Login, And Templating Contd.

--- ums/urls.py
+++ ums/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index, name="Homepage"),
+    path('',views.index, name="homepage"),
+    path('login',views.sign_in, name="sign_in"),
+    path('home',views.user_homepage, name="user_homepage"),
 ]
 
