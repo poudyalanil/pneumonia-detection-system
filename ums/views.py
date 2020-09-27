a28ec6e0b805b17dd3609db8e20fd11c2fbc5b1c
poudyalanil <anilrunjakote@gmail.com>
2020-09-27 08:22:37 +0545

    View Structure

--- ums/views.py
+++ ums/views.py
@@ -6,22 +6,92 @@ from django.contrib.auth import  authenticate
 from django.contrib import messages
 # Create your views here.
 
+
+######################
+##### general   ####
+######################
 def index(requests):
+    #url: /
     return render(requests,'ums/index.html')
 
 def sign_in(requests):
+      #url: /login
     if requests.POST:
         username = requests.POST['username']
         password = requests.POST['password']
         auth_user = authenticate(username=username,password=password)
         if auth_user:
             messages.success(requests,"Login Success")
-            request.session['username'] = username
+            requests.session['username'] = username
+
+            return redirect('user_homepage')
             # TODO
         else:
             messages.error(requests,"Incorrect Credentials")
 
     return render(requests,'ums/login.html')
 
+def logout(requests):
+      #url: /logout/<id>
+      #TODO
+      pass
+
+
+
+######################
+##### user side   ####
+######################
+
 def user_homepage(requests):
+      #url: /usr/home
+      #TODO
     return render(requests,'ums/dashboard.html')
+
+def all_patients(requests):
+      #url: /usr/patients/
+      #TODO
+      pass
+
+def new_test(requests):
+      #url: /usr/new/
+      #TODO
+      pass
+
+def stats(requests):
+      #url: /usr/stats/
+      #TODO
+      pass
+
+def support(requests):
+      #url: /usr/support/
+      #TODO
+      pass
+
+def usr_profile(requests):
+      #url: /usr/profile/<id>
+      #TODO
+      pass
+
+
+
+
+######################
+##### admin side  ####
+######################
+
+def new_user(requests):
+      #url: /admin/new-usr/
+      #TODO
+    return render(requests,"ums/new_user.html")
+
+def delete_usr(requests):
+      #url: /admin/usr/del/<id>
+        #TODO
+      pass
+def edit_usr(request):
+      #url: /admin/usr/edit/<id>
+      #TODO
+      pass
+
+
+
