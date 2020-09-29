4f879a5c42ae9a692a5d344d322b00b6f9e4ea94
poudyalanil <anilrunjakote@gmail.com>
2020-09-29 18:49:37 +0545

    Edit User

--- ums/forms.py
+++ ums/forms.py
@@ -24,3 +24,24 @@ class Normal_User_Form(forms.ModelForm):
     class Meta:
         model = Normal_User
         fields = ['phone', 'gender', 'country','profile_pic']
+
+class User_Update_Form(forms.ModelForm):
+    email = forms.EmailField(required=True)
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'email', 'password']
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+
+        user.set_password(self.cleaned_data['password'])
+        if commit:
+            user.save()
+            return user
+
+class Normal_User_Update_Form(forms.ModelForm):
+    class Meta:
+        model = Normal_User
+        fields = ['phone', 'gender', 'country','profile_pic']
