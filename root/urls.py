ffcaa080a9d8f63a4aa9f02124fe3d79de31c9ff
poudyalanil <anilrunjakote@gmail.com>
2020-09-29 17:34:34 +0545

    UMS Crispy Forms, Admin Dashboard

--- root/urls.py
+++ root/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('adminmgmt/', admin.site.urls),
     path('',include('ums.urls')),
 ]
 if settings.DEBUG:
