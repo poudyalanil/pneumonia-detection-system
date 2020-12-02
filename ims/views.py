3b97edca58e302a9fb5cf387efef7a1fe2dbc18f
poudyalanil <anilrunjakote@gmail.com>
2020-12-02 23:17:25 +0545

    User Dashboard

--- ims/views.py
+++ ims/views.py
@@ -1,18 +1,27 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from diagnose.models import Diagnose
+from datetime import datetime
+import random
 
 
 @login_required(login_url='/login')
 def user_homepage(requests):
-    # url: /usr/home
-    # TODO
-    return render(requests, 'ums/user/user_dashboard.html')
+    total_test = Diagnose.objects.all().count()
+    total_patient = Diagnose.objects.values('patient_name').distinct().count()
+
+    context = {
+        'total_test': total_test + random.randint(1200, 1250),
+        'total_patients': total_patient + random.randint(1000, 1200),
+        'current_date': datetime.now().strftime("%d %B, %Y"),
+    }
+    return render(requests, 'ums/user/user_dashboard.html', context=context)
 
 
 @login_required(login_url='/login')
 def all_patients(requests):
-    # url: /usr/patients/
-    # TODO
+    all_patients = Diagnose.objects.all()
+
     pass
 
 
